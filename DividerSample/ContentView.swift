0214025aaab7b4197ecd825343da2c69cfffd911
SamplesMaker <insert@kakao.com>
2020-03-02 08:43:47 +0900

    Initial Commit

---
+++ DividerSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DividerSample
+//
+//  Created by iDev0 on 2020/03/02.
+//  Copyright © 2020 Ju Young Jung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
