92cc3bc789a2521b58278462732d7e44b899229b
SamplesMaker <insert@kakao.com>
2020-03-02 13:51:42 +0900

    Add Divider Sample

--- DividerSample/ContentView.swift
+++ DividerSample/ContentView.swift
@@ -10,7 +10,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            
+            Group {
+                Text("horizontal Divider")
+                Divider()
+                Image(systemName: "1.circle")
+                Divider()
+            }
+            
+            Group {
+                Text("Vertical Divider")
+                
+                HStack {
+                    Divider()
+                    Image(systemName: "2.circle")
+                    Divider()
+                }.frame(height: 100)
+                
+                Group {
+                    // 3.
+                    Text("Change Divider Color")
+                    Image(systemName: "3.circle")
+                    Divider().background(Color.red)
+                    
+                    // 4.
+                    Text("Change Divider Size")
+                    Image(systemName: "4.circle")
+                    Divider().frame(width: 200)
+                }
+
+            }
+            
+            
+        }
+        
+        
+        
     }
 }
 
